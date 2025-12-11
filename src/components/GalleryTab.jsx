959b8d166740a9b009eafc20fcc15d5b9382c300
felix-schmitt <f-schmitt@gmx.ch>
2025-12-11 15:14:04 +0100

    make images clickable to show them bigger

--- src/components/GalleryTab.jsx
+++ src/components/GalleryTab.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import {Figure, FigureCaption, FigureImage} from "react-bootstrap";
+import React, { useState } from 'react';
+import { Figure, FigureCaption, FigureImage, Modal } from "react-bootstrap";
 
 function GalleryTab({ title, description, data, folder }) {
+    const [show, setShow] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleOpen = (image, title) => {
+        setSelectedImage({ image, title });
+        setShow(true);
+    };
+
     return (
         <div style={{ marginTop: 30 }}>
             <h3>{title}</h3>
             {description != null || description === "" ? <p>{description}</p> : null}
+
             {data.map((item, index) => (
-                <Figure key={index} style={{ marginBottom: 40 }}>
+                <Figure
+                    key={index}
+                    className="figureComponent"
+                    onClick={() => handleOpen(item.image, item.title)}
+                >
                     <FigureImage
                         src={`${folder}/images/${item.image}`}
                         alt={item.title}
-                        style={{ width: 300, borderRadius: "8px" }}
                     />
                     <FigureCaption>
                         <h3>{item.title}</h3>
@@ -19,6 +31,20 @@ function GalleryTab({ title, description, data, folder }) {
                     </FigureCaption>
                 </Figure>
             ))}
+
+            {/* Modal for enlarged image */}
+            <Modal show={show} onHide={() => setShow(false)} centered size="lg">
+                <Modal.Header closeButton>
+                    {selectedImage ? selectedImage.title : ""}
+                </Modal.Header>
+                <Modal.Body style={{ textAlign: "center" }}>
+                    {selectedImage ? <img
+                            src={`${folder}/images/${selectedImage.image}`}
+                            alt={selectedImage.title}
+                            className="figureModal"
+                        /> : null}
+                </Modal.Body>
+            </Modal>
         </div>
     );
 }
