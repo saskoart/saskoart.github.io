a93d57c70c358022cd6f4e8d212862e4db9d6551
felix-schmitt <f-schmitt@gmx.ch>
2026-02-06 18:51:05 +0100

    prepared footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Footer() {
     return <footer className="footer">
-        <p>Footer placeholder</p>
-
+        <p>© SASKO ART</p>
+        <a href="javascript:window.document.cookieBanner.show()">Cookie-Banner anzeigen</a>
+        <p>Impressum</p>
+        <p>Datenschutzerklärung</p>
     </footer>
 }
 
