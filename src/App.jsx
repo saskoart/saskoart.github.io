a48aa96c5f137eef905b8ab81f7469282cfa8bff
felix-schmitt <f-schmitt@gmx.ch>
2026-03-01 14:29:59 +0100

    added redirect fix for github

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,15 @@ function App() {
     const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const redirect = params.get("redirect");
+
+        if (redirect) {
+            window.history.replaceState(null, "", redirect);
+        }
+    }, []);
+
+    useEffect(() => {
         loadStructure(setStructure);
     }, []);
 
